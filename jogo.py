be959f0288f7b20901202d5336181ab9e4b4cda4
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2023-12-19 20:40:56 -0300

    Colisão e obstáculos "arrumados"
    Falta mexer na velocidade e ajustar a pontuação para reagir de acordo com a velocidade

--- jogo.py
+++ jogo.py
@@ -4,63 +4,71 @@ import random
 pygame.font.init()
 
 # Window size
-WIDTH, HEIGHT = 1000, 800
+WIDTH, HEIGHT = 1800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dino Game")
 
+imagem = pygame.image.load('dinoSpritesheet.png')
+
+
 # player infos
-PLAYER_IMG = pygame.transform.scale(pygame.image.load('img_dinossauro.png'), (100, 100)).convert_alpha()
-PLAYER_WIDTH, PLAYER_HEIGHT = 100, 100
+PLAYER_IMG = pygame.transform.scale(pygame.image.load('img_dinossauro.png'), (130, 120)).convert_alpha()
+PLAYER_WIDTH, PLAYER_HEIGHT = 90, 80
 PLAYER_VEL = 5
 
 # obstacle infos
-OBSTACLE_WIDTH, OBSTACLE_HEIGHT = 30, 100
+OBSTACLE_WIDTH, OBSTACLE_HEIGHT = 20, 50
 OBSTACLE_VEL = 5
+cacto = imagem.subsurface((32*5, 0, 32, 32))
+cacto = pygame.transform.scale(cacto, (OBSTACLE_WIDTH + 20, OBSTACLE_HEIGHT))
 
-def draw(dino, obstaculos):
+def draw(dino, obstaculos, score):
     WIN.fill('white')
     
     WIN.blit(PLAYER_IMG, (dino.x, dino.y))
     
     for obs in obstaculos:
-        pygame.draw.rect(WIN, 'green', (obs.x, obs.y, obs.width, obs.height))
+        # pygame.draw.rect(WIN, 'green', (obs.x, obs.y, obs.width, obs.height))
+        WIN.blit(cacto, (obs.x, obs.y))
+    
+    # pontuação
+    font = pygame.font.SysFont('comicsans', 30)
+    text = font.render('Pontuação: ' + str(score), 1, (0, 0, 0))
+    WIN.blit(text, (10, 50))
     
     
     pygame.display.update()
 
-class Dino():
+class Dino(pygame.Rect):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.width = PLAYER_WIDTH
-        self.height = PLAYER_HEIGHT
+        super().__init__(x, y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.vel = PLAYER_VEL
         self.img = PLAYER_IMG
         self.pulo = False
     
     def pular(self):
         if self.pulo == False:
-            self.y -= 200
+            self.y -= 250
             self.pulo = True
     
     def gravidade(self):
             if self.y < 500:
-                self.y += 10
+                self.y += 5
             else:
                 self.pulo = False
+    
+    def abaixar(self):
+        self.y = 500
 
-class Obstaculo():
+class Obstaculo(pygame.Rect):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.width = OBSTACLE_WIDTH
-        self.height = OBSTACLE_HEIGHT
+        super().__init__(x, y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)
         self.vel = OBSTACLE_VEL
+
     
     def move(self):
-        self.x -= self.vel
+        self.x -= self.vel       
 
-                  
 
 
 def main():
@@ -69,9 +77,12 @@ def main():
     clock = pygame.time.Clock()
     obstaculos = []
     obs_timer = 0
+    max_obs = 7
+    pontuacao = 0
     while run:
         clock.tick(60)
         obs_timer += 1
+        pontuacao += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -81,18 +92,30 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             dino.pular()
+        # Agachar
+        if keys[pygame.K_DOWN]:
+            dino.abaixar()
 
         dino.gravidade()
         
         # gerando obstaculos
-        # if obs_timer > 20:
-        if random.randint(0, 10) == 1:
-            obstaculos.append(Obstaculo(WIDTH, 500))
-            obs_timer -= 10
+        if len(obstaculos) == 0 or obstaculos[-1].x < random.randint(500, 1000):
+            if obs_timer > random.randint(100, 500) and len(obstaculos) <= max_obs and obs_timer % 10 == 0:
+                obstaculos.append(Obstaculo(WIDTH, 550))
+                obs_timer -= 100
         for obs in obstaculos:
             obs.move()
+            if obs.colliderect(dino):
+                run = False
+                break
+            if obs.x < -OBSTACLE_WIDTH:
+                obstaculos.remove(obs)
+        
+        if run == False:
+            break
+            
 
-        draw(dino, obstaculos)
+        draw(dino, obstaculos, pontuacao)
             
     pygame.quit()
 
