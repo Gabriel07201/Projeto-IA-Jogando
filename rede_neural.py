5a28e14f299ac626c139d260f83702f63f4d42e7
Gabriel Antonio de Lima <105071638+Gabriel07201@users.noreply.github.com>
2023-12-22 19:24:11 -0300

    add_optimzer

--- rede_neural.py
+++ rede_neural.py
@@ -1,5 +1,8 @@
+import torch
 from torch import nn
 from torch import optim
+import torch.nn.functional as F
+import os
 
 # criando uma rede com 5 neuronios, 1 camada oculta com 5 neuronios e 2 outputs
 # na entrada ela vai receber os seguintes dados: obstaculo distancia, obstaculo largura, obstaculo altura, velocidade do obstaculo, altura do dino
@@ -24,9 +27,45 @@ class RedeNeural(nn.Module):
         return output
 
 
-def otimizando_rede(model, n_novas_redes):
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    def save(self, file_name='model.pht'):
+        model_folder_path = './model'
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
     
-    # otimizando a melhor rede e gerando novas redes
-    
+    def train_step(self, state, action, reward, next_state, game_over):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        
+        if len(state.shape) == 1:
+            # adicionando uma dimensão
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            game_over = (game_over, )
+        
+        pred = self.model(state)
+        
+        target = pred.clone()
+        for idx in range(len(game_over)):
+            Q_new = reward[idx]
+            if not game_over[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+            
+            target[idx][torch.argmax(action).item()] = Q_new
+        
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+        
+        self.optimizer.step()
